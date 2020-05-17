d745a6528bfaadc9f635491e4fc24e913003afde
unknown <kimtangs@gmail.com>
2020-05-17 13:51:47 +0900

    Updates

---
+++ precache-manifest.eb5067ac748afb706ceaede77337da8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d159453dcc520fcab08fb3ceaebc405",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "c25fb50af996955de5a4",
+    "url": "/movie_app/static/css/main.0964ac2e.chunk.css"
+  },
+  {
+    "revision": "4198c32a3feb3286d27a",
+    "url": "/movie_app/static/js/2.7bc66b43.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.7bc66b43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c25fb50af996955de5a4",
+    "url": "/movie_app/static/js/main.f0d448f5.chunk.js"
+  },
+  {
+    "revision": "71efb74f876f07c96a40",
+    "url": "/movie_app/static/js/runtime-main.78ea198c.js"
+  }
+]);
